48b90b664a42a8913b79d0f9b37cb60b05e033d4
Richard Hung <richardhung111@gmail.com>
2018-05-09 23:02:09 -0700

    Item type check

--- src/parseDirectory.js
+++ src/parseDirectory.js
@@ -21,6 +21,21 @@ exports.loopDir = function(path, program) {
     files.forEach( function( file, index ) {
       var filepath = path + file;
 
+      // Check filetype
+      fs.lstat(filepath, (err, stats) => {
+        if(err) {
+          return console.log(err); //Handle error
+        }
+
+
+        // console.log(`Is file: ${stats.isFile()}`);
+        // console.log(`Is directory: ${stats.isDirectory()}`);
+        // console.log(`Is symbolic link: ${stats.isSymbolicLink()}`);
+        // console.log(`Is FIFO: ${stats.isFIFO()}`);
+        // console.log(`Is socket: ${stats.isSocket()}`);
+        // console.log(`Is character device: ${stats.isCharacterDevice()}`);
+        // console.log(`Is block device: ${stats.isBlockDevice()}`);
+      });
     } );
   } );
 
