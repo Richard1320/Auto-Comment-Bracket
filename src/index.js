43bb8ef1b9a26ceef09c6461d01d199d850cbf0a
Richard Hung <richardhung111@gmail.com>
2018-05-09 22:54:17 -0700

    loop directory option

--- src/index.js
+++ src/index.js
@@ -5,21 +5,25 @@
 
 var program = require('commander');
 var parseFile = require('./parseFile.js');
+var parseDirectory = require('./parseDirectory.js');
 
 
 program
-.arguments('<file>')
-.option('-o, --output <filename>', 'The output file to be written')
+.arguments('<path>')
+.version('1.0.3', '-v, --version')
+.option('-o, --output <path>', 'The output path to be written')
 .option('-u, --undo', 'Remove comments added using this module')
+.option('-d, --directory', 'Loop through all files in directory')
+.option('-r, --recursive', 'Loop through all files in subdirectories')
 // .option('-p, --password <password>', 'The user\'s password')
-.action(function(file) {
-  if (program.undo) {
-    parseFile.undoFile(file,program.output);
+.action(function(path) {
+  // console.log(program);
+
+  // Check if looping through directory
+  if (program.directory) {
+    parseDirectory.loopDir(path,program);
   } else {
-    parseFile.processFile(file,program.output);
+    parseFile.processFile(path,program);
   }
-  // console.log(program.output);
-  //  console.log('user: %s pass: %s file: %s',
-  //      program.username, program.password, file);
 })
 .parse(process.argv);
