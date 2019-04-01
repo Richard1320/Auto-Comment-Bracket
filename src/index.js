62b281a531022c550d8316287cd28e1121c99dbb
Richard Hung <richardhung111@gmail.com>
2019-03-31 18:15:23 -0700

    version number 1.1.4

--- src/index.js
+++ src/index.js
@@ -2,29 +2,27 @@
 
 'use strict';
 
-
 var program = require('commander');
 var parseFile = require('./parseFile.js');
 var parseDirectory = require('./parseDirectory.js');
 
-
 program
-.arguments('<path>')
-.version('1.1.3', '-v, --version')
-.option('-o, --output <path>', 'The output path to be written')
-.option('-e, --exclude <path>', 'The output path to be written')
-.option('-u, --undo', 'Remove comments added using this module')
-.option('-d, --directory', 'Loop through all files in directory')
-.option('-r, --recursive', 'Loop through all files in subdirectories')
-// .option('-p, --password <password>', 'The user\'s password')
-.action(function(path) {
-  // console.log(program);
+  .arguments('<path>')
+  .version('1.1.4', '-v, --version')
+  .option('-o, --output <path>', 'The output path to be written')
+  .option('-e, --exclude <path>', 'The output path to be written')
+  .option('-u, --undo', 'Remove comments added using this module')
+  .option('-d, --directory', 'Loop through all files in directory')
+  .option('-r, --recursive', 'Loop through all files in subdirectories')
+  // .option('-p, --password <password>', 'The user\'s password')
+  .action(function(path) {
+    // console.log(program);
 
-  // Check if looping through directory
-  if (program.directory) {
-    parseDirectory.loopDir(path,program);
-  } else {
-    parseFile.processFile(path,program);
-  }
-})
-.parse(process.argv);
+    // Check if looping through directory
+    if (program.directory) {
+      parseDirectory.loopDir(path, program);
+    } else {
+      parseFile.processFile(path, program);
+    }
+  })
+  .parse(process.argv);
