00f0de5ee8c0d2320fcbfa1bd8b851cd3f354206
Richard <richardhung111@gmail.com>
2019-11-12 22:02:33 -0800

    Separate path arguments for input, output & exclude

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,21 @@ import {loopDir} from "./parseDirectory";
 import {processFile} from "./parseFile";
 
 program
-	.arguments('<path>')
+	.arguments('<inputPath>')
 	.version('1.1.5', '-v, --version')
-	.option('-o, --output <path>', 'The output path to be written')
-	.option('-e, --exclude <path>', 'Excluded file path from directory loop')
+	.option('-o, --output <outputPath>', 'The output path to be written')
+	.option('-e, --exclude <excludePath>', 'Excluded file path from directory loop')
 	.option('-u, --undo', 'Remove comments added using this module')
 	.option('-d, --directory', 'Loop through all files in directory')
 	.option('-r, --recursive', 'Loop through all files in subdirectories')
-	.action(function (path) {
+	.action(function (inputPath) {
 		// console.log(program);
 
 		// Check if looping through directory
 		if (program.directory) {
-			loopDir(path, program);
+			loopDir(inputPath, program);
 		} else {
-			processFile(path, program);
+			processFile(inputPath, program);
 		}
 	})
 	.parse(process.argv);
